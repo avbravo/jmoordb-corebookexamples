c86a0849bc6cdb73a86329fe669bccef14568fbb
avbravo <avbravo@avbravo>
2022-09-19 16:18:57 -0500

    1.0.3

--- capitulo02/src/main/java/com/jmoordbcore/capitulo02/repository/PaisRepository.java
+++ capitulo02/src/main/java/com/jmoordbcore/capitulo02/repository/PaisRepository.java
@@ -1,19 +1,18 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package com.jmoordbcore.capitulo02.repository;
 
 import com.jmoordb.core.annotation.enumerations.JakartaSource;
 import com.jmoordb.core.annotation.repository.Repository;
 import com.jmoordb.core.repository.CrudRepository;
-import com.jmoordbcore.capitulo02.model.Oceano;
- 
+import com.jmoordbcore.capitulo02.model.Pais;
+
 /**
  *
  * @author avbravo
  */
-@Repository(entity = Oceano.class,jakartaSource = JakartaSource.JAVAEE_LEGACY)
-public interface OceanoRepository extends CrudRepository<Oceano, String> {
-    
+@Repository(entity = Pais.class, jakartaSource = JakartaSource.JAVAEE_LEGACY)
+public interface PaisRepository extends CrudRepository<Pais, Long>{     
 }
