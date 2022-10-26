10a2039252bbbe6e115d6c07131adb1089d5afb4
avbravo <avbravo@avbravo>
2022-10-26 17:16:13 -0500

    capitulo13

---
+++ capitulo13/run.sh
@@ -0,0 +1,3 @@
+mvn clean install payara-micro:bundle
+
+java -jar target/capitulo13-microbundle.jar 
---
+++ capitulo13/src/main/java/com/jmoordbcore/capitulo02/controller/OceanoController.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.jmoordbcore.capitulo02.controller;
+
+import com.jmoordbcore.capitulo02.model.Oceano;
+import com.jmoordbcore.capitulo02.repository.OceanoRepository;
+import java.util.List;
+import javax.inject.Inject;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+
+/**
+ *
+ * @author avbravo
+ */
+@Path("oceano")
+public class OceanoController {
+    @Inject
+    OceanoRepository oceanoRepository;
+    
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<Oceano> findAll(){
+        return oceanoRepository.findAll();
+    }
+    
+    @GET
+    public String sayHello() {
+        return "Hello World";
+    }
+}
