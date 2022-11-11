cf7f725a31858ae96aa6982c0ea709931911189b
avbravo <avbravo@avbravo>
2022-11-11 09:43:59 -0500

    0.2

---
+++ capitulo0802/src/main/java/com/jmoordbcore/producer/MongoDBProducer.java
@@ -0,0 +1,45 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.jmoordbcore.producer;
+
+/**
+ *
+ * @author avbravo
+ */
+import com.jmoordb.core.annotation.DateSupport;
+
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import java.io.Serializable;
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.enterprise.inject.Disposes;
+import jakarta.enterprise.inject.Produces;
+import jakarta.inject.Inject;
+import org.eclipse.microprofile.config.Config;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+
+@ApplicationScoped
+@DateSupport()
+public class MongoDBProducer implements Serializable {
+
+    @Inject
+    private Config config;
+    @Inject
+    @ConfigProperty(name = "mongodb.uri")
+    private String mongodburi;
+    
+    @Produces
+    @ApplicationScoped
+    public MongoClient mongoClient() {
+        MongoClient mongoClient = MongoClients.create(mongodburi);
+       return mongoClient;
+
+    }
+
+    public void close(@Disposes final MongoClient mongoClient) {
+        mongoClient.close();
+    }
+
+}
