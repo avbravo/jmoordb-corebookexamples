03a4c7ffb8b1a53fb2ce2dda75c9d0d3727756aa
avbravo <avbravo@avbravo>
2022-11-16 12:43:42 -0500

    capitulo 17

---
+++ capitulo17/src/main/java/com/jmoordbcore/capitulo17/health/ServiceReadyHealthCheck.java
@@ -0,0 +1,19 @@
+package com.jmoordbcore.capitulo17.health;
+
+import org.eclipse.microprofile.health.HealthCheck;
+import org.eclipse.microprofile.health.HealthCheckResponse;
+import org.eclipse.microprofile.health.Readiness;
+
+import jakarta.enterprise.context.ApplicationScoped;
+
+@Readiness
+@ApplicationScoped
+public class ServiceReadyHealthCheck implements HealthCheck {
+
+    @Override
+    public HealthCheckResponse call() {
+
+        return HealthCheckResponse.named(ServiceReadyHealthCheck.class.getSimpleName()).withData("ready",true).up().build();
+
+    }
+}
---
+++ capitulo17/src/main/java/com/jmoordbcore/capitulo17/health/ServiceStartupHealthCheck.java
@@ -0,0 +1,19 @@
+package com.jmoordbcore.capitulo17.health;
+
+import org.eclipse.microprofile.health.HealthCheck;
+import org.eclipse.microprofile.health.HealthCheckResponse;
+import org.eclipse.microprofile.health.Startup;
+
+import jakarta.enterprise.context.ApplicationScoped;
+
+@Startup
+@ApplicationScoped
+public class ServiceStartupHealthCheck implements HealthCheck {
+
+    @Override
+    public HealthCheckResponse call() {
+
+        return HealthCheckResponse.named(ServiceStartupHealthCheck.class.getSimpleName()).withData("startup",true).up().build();
+
+    }
+}
