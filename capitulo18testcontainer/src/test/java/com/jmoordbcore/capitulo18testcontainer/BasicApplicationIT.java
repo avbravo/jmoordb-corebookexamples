4b598f05156633c9bed9cc0a95d83785f2813cde
avbravo <avbravo@avbravo>
2022-11-25 22:34:29 -0500

    0.2.100

---
+++ capitulo18testcontainer/src/test/java/com/jmoordbcore/capitulo18testcontainer/BasicApplicationIT.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.jmoordbcore.capitulo18testcontainer;
+
+
+import org.junit.jupiter.api.Test;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import static org.testcontainers.shaded.org.apache.commons.lang.BooleanUtils.isTrue;
+
+/**
+ *
+ * @author avbravo
+ */
+@Testcontainers
+public class BasicApplicationIT{
+  @Container
+  GenericContainer microContainer = 
+    new GenericContainer("payara/micro-jdk11:5.2022.1")
+                    .withExposedPorts(8080);
+  @Test
+  public void checkContainerIsRunning(){
+      assert(isTrue(microContainer.isRunning()));
+  }
+}
---
+++ capitulo18testcontainer/src/test/java/com/jmoordbcore/capitulo18testcontainer/HelloControllerIT.java
@@ -0,0 +1,33 @@
+package com.jmoordbcore.capitulo18testcontainer;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit5TestClass.java to edit this template
+ */
+/**
+ *
+ * @author avbravo
+ */
+//@MicroShedTest
+public class HelloControllerIT {
+//  @Container
+//    public static MicroProfileApplication app = new MicroProfileApplication();
+//
+////   @Container
+////    public static ApplicationContainer app = new ApplicationContainer()
+////                    .withAppContextRoot("/guide-microshed-testing")
+////                    .withReadinessPath("/health/ready");
+//  
+//    @Inject
+//    public static HelloController helloController;
+//
+//    @Test
+//    public void sayHello() throws IOException {
+//        String data =  helloController.sayHello();
+//
+//        assertThat(data).startsWith("Hello World");
+//        System.out.println(data);
+//
+//    }
+
+}
