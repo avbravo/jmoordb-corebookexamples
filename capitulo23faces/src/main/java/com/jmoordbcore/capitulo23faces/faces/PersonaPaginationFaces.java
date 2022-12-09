7d3a96854d27eaa8a1f5e0993b9a9d11a5f21b92
avbravo <avbravo@avbravo>
2022-12-09 13:43:15 -0500

    0.1

--- capitulo23faces/src/main/java/com/jmoordbcore/capitulo23faces/faces/PersonaPaginationFaces.java
+++ capitulo23faces/src/main/java/com/jmoordbcore/capitulo23faces/faces/PersonaPaginationFaces.java
@@ -4,20 +4,31 @@
  */
 package com.jmoordbcore.capitulo23faces.faces;
 
+import com.avbravo.jmoordbutils.paginator.IPaginator;
+import com.avbravo.jmoordbutils.paginator.Paginator;
 import com.jmoordb.core.model.Pagination;
-import com.jmoordb.core.util.JmoordbCorePageUtil;
-import com.jmoordb.core.util.JmoordbCoreUtil;
-import com.jmoordbcore.capitulo23faces.datamodel.PersonaDataModel;
+import com.jmoordb.core.util.DocumentUtil;
 import com.jmoordbcore.capitulo23faces.model.Persona;
 import com.jmoordbcore.capitulo23faces.repository.PersonaRepository;
 import jakarta.annotation.PostConstruct;
 import jakarta.faces.view.ViewScoped;
 import jakarta.inject.Inject;
 import jakarta.inject.Named;
+import jakarta.inject.Provider;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
+import static java.util.Locale.filter;
+import java.util.Map;
 import lombok.Data;
+import org.bson.Document;
+import org.bson.conversions.Bson;
+import org.eclipse.microprofile.config.Config;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+import org.primefaces.PrimeFaces;
+import org.primefaces.model.FilterMeta;
+import org.primefaces.model.LazyDataModel;
+import org.primefaces.model.SortMeta;
 
 /**
  *
@@ -26,15 +37,41 @@ import lombok.Data;
 @Named()
 @ViewScoped
 @Data
-public class PersonaPaginationFaces implements Serializable {
+public class PersonaPaginationFaces implements Serializable, IPaginator {
 
     private static final long serialVersionUID = 1L;
     @Inject
     PersonaRepository personaRepository;
 
-    private PersonaDataModel personaDataModel = new PersonaDataModel();
     List<Persona> personaList = new ArrayList<>();
 
+    // <editor-fold defaultstate="collapsed" desc="LazyDataModel>
+    private LazyDataModel<Persona> personaLazyDataModel;
+// </editor-fold>
+
+    // <editor-fold defaultstate="collapsed" desc="paginator ">
+    Paginator paginator = new Paginator();
+    Paginator paginatorOld = new Paginator();
+
+    public Paginator getPaginator() {
+        return paginator;
+    }
+
+    public void setPaginator(Paginator paginator) {
+        this.paginator = paginator;
+    }
+
+    // </editor-fold>
+    // <editor-fold defaultstate="collapsed" desc="Microprofile Config">
+    @Inject
+    private Config config;
+
+    // Row
+    @Inject
+    @ConfigProperty(name = "rowPage", defaultValue = "0")
+    private Provider<Integer> rowPage;
+
+    // </editor-fold>
     /**
      * Creates a new instance of PersonaFaces
      */
@@ -45,29 +82,84 @@ public class PersonaPaginationFaces implements Serializable {
     // <editor-fold defaultstate="collapsed" desc=" init">
     @PostConstruct
     public void init() {
+
         findAllPagination();
-    }
-// </editor-fold>
 
-  
-    public String findAllPagination() {
-        try {
-            Long count = personaRepository.count();
+        this.personaLazyDataModel = new LazyDataModel<Persona>() {
+            @Override
+            public List<Persona> load(int offset, int pageSize, Map<String, SortMeta> sortBy, Map<String, FilterMeta> filterBy) {
+                System.out.println("Load.....");
+                Integer totalRecords = personaRepository.count().intValue();
+                System.out.println(" [] load 1 " + totalRecords);
 
-            Integer numberOfPage = JmoordbCorePageUtil.numberOfPages(JmoordbCoreUtil.longToInteger(count), 25);
+                List<Paginator> list = processLazyDataModel(paginator, paginatorOld, offset, rowPage.get(), totalRecords, sortBy);
+                System.out.println(" [] load 2 ");
+                paginator = list.get(0);
+                paginatorOld = list.get(1);
+                paginator.setNumberOfPage(numberOfPages(totalRecords, rowPage.get()));
+                System.out.println(" [] load 3 ");
 
-            System.out.println("Number OfPage  "+numberOfPage);
+                Pagination pagination = new Pagination(paginator.getPage(), rowPage.get());
+                System.out.println(" [] load 4 ");
+//                System.out.println("-->Pagintation  paginator.getPage() ["+paginator.getPage()+"]  [paginator.getNumberOfPage()] " + paginator.getNumberOfPage());
+                System.out.println("-->Pagintation  paginator.getPage() ["+paginator.getPage()+"]  [paginator.getNumberOfPage()] " + paginator.getNumberOfPage());
+                
+                
+                
+                List<Persona> result = personaRepository.findAllPagination(pagination);
+                System.out.println(" [] load 5 ");
+                personaLazyDataModel.setRowCount(totalRecords);
+                System.out.println(" [] load 6 ");
+                PrimeFaces.current().executeScript("setDataTableWithPageStart()");
+                System.out.println(" [] load 7 ");
+                return result;
+            }
 
-            Pagination pagination = new Pagination(1, 25);
+            @Override
+            public int count(Map<String, FilterMeta> map) {
+                System.out.println("----------------------Metodo count");
+                Integer totalRecords2 = personaRepository.count().intValue();
+                System.out.println("----------------Total records 2 " + totalRecords2);
+                return totalRecords2;
+            }
 
+        };
+
+        //  Long count = personaRepository.count();
+//            Integer numberOfPage = JmoordbCorePageUtil.numberOfPages(JmoordbCoreUtil.longToInteger(count), 25);
+//
+//            System.out.println("Number OfPage  " + numberOfPage);
+//
+//            Pagination pagination = new Pagination(1, 25);
 //            pagination = JmoordbCorePageUtil.first(pagination);
 //            pagination = JmoordbCorePageUtil.last(pagination);
 //            pagination = JmoordbCorePageUtil.next(pagination);
 //            pagination = JmoordbCorePageUtil.back(pagination);
 //            personaList = personaRepository.findAll();
+//            move(pagination);
+    }
+// </editor-fold>
 
-            move(pagination);
-          
+    public String findAllPagination() {
+        try {
+            System.out.println(">>>>>>>>>>>>>>>>>>><<<< paso 0");
+            
+//                Bson filter
+//                    = DocumentUtil.createBsonBetweenDateWithoutHours(
+//                            "fechahora", startDate, "fechahora", endDate);
+//                
+              Bson filter = new Document();
+                
+            Document sort = new Document("idpersona", -1);
+            paginator
+                    = new Paginator.Builder()
+                            .page(1)
+                            .query(DocumentUtil.jsonToDocument(DocumentUtil.bsonToJson(filter)))
+                            .query(new Document())
+                            .sort(new Document())
+                            .title("Filtro basico")
+                            .build();
+
         } catch (Exception e) {
             System.out.println("findAll() " + e.getLocalizedMessage());
         }
@@ -75,19 +167,4 @@ public class PersonaPaginationFaces implements Serializable {
         return "";
     }
 
-    
-    public String move(Pagination pagination){
-          personaList = personaRepository.findAllPagination(pagination);
-            if (personaList == null) {
-                System.out.println(">>>> No hay registros......");
-            } else {
-                
-                   personaDataModel = new PersonaDataModel(personaList);
-                System.out.println("Hay registros.......");
-                for (Persona p : personaList) {
-                    System.out.println("p.getNombre():  " + p.getNombre() + " p.getIdpersona(): " + p.getIdpersona());
-                }
-            }
-            return "";
-    }
 }
---
+++ capitulo23faces/src/main/java/com/jmoordbcore/capitulo23faces/faces/PersonaPaginationFaces1.java
@@ -0,0 +1,93 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSF/JSFManagedBean.java to edit this template
+ */
+package com.jmoordbcore.capitulo23faces.faces;
+
+import com.jmoordb.core.model.Pagination;
+import com.jmoordb.core.util.JmoordbCorePageUtil;
+import com.jmoordb.core.util.JmoordbCoreUtil;
+import com.jmoordbcore.capitulo23faces.datamodel.PersonaDataModel;
+import com.jmoordbcore.capitulo23faces.model.Persona;
+import com.jmoordbcore.capitulo23faces.repository.PersonaRepository;
+import jakarta.annotation.PostConstruct;
+import jakarta.faces.view.ViewScoped;
+import jakarta.inject.Inject;
+import jakarta.inject.Named;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import lombok.Data;
+
+/**
+ *
+ * @author avbravo
+ */
+@Named()
+@ViewScoped
+@Data
+public class PersonaPaginationFaces1 implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Inject
+    PersonaRepository personaRepository;
+
+    private PersonaDataModel personaDataModel = new PersonaDataModel();
+    List<Persona> personaList = new ArrayList<>();
+
+    /**
+     * Creates a new instance of PersonaFaces
+     */
+    public PersonaPaginationFaces1() {
+
+    }
+
+    // <editor-fold defaultstate="collapsed" desc=" init">
+    @PostConstruct
+    public void init() {
+        findAllPagination();
+    }
+// </editor-fold>
+
+  
+    public String findAllPagination() {
+        try {
+            Long count = personaRepository.count();
+
+            Integer numberOfPage = JmoordbCorePageUtil.numberOfPages(JmoordbCoreUtil.longToInteger(count), 25);
+
+            System.out.println("Number OfPage  "+numberOfPage);
+
+            Pagination pagination = new Pagination(1, 25);
+
+//            pagination = JmoordbCorePageUtil.first(pagination);
+//            pagination = JmoordbCorePageUtil.last(pagination);
+//            pagination = JmoordbCorePageUtil.next(pagination);
+//            pagination = JmoordbCorePageUtil.back(pagination);
+//            personaList = personaRepository.findAll();
+
+            move(pagination);
+          
+        } catch (Exception e) {
+            System.out.println("findAll() " + e.getLocalizedMessage());
+        }
+
+        return "";
+    }
+
+    
+    public String move(Pagination pagination){
+          personaList = personaRepository.findAllPagination(pagination);
+            if (personaList == null) {
+                System.out.println(">>>> No hay registros......");
+            } else {
+                
+                   personaDataModel = new PersonaDataModel(personaList);
+                System.out.println("Hay registros.......");
+                for (Persona p : personaList) {
+                    System.out.println("p.getNombre():  " + p.getNombre() + " p.getIdpersona(): " + p.getIdpersona());
+                }
+            }
+            return "";
+    }
+}
