abacbf41d1b3fd7fcc32d71b905bcc188469b4b6
avbravo <avbravo@avbravo-HP-Pavilion-g7-Notebook-PC>
2023-07-26 21:01:14 -0500

    capitulo 08

--- capitulo08/src/main/java/com/jmoordbcore/capitulo08/repository/AnimalRepository.java
+++ capitulo08/src/main/java/com/jmoordbcore/capitulo08/repository/AnimalRepository.java
@@ -4,10 +4,24 @@
  */
 package com.jmoordbcore.capitulo08.repository;
 
+import com.jmoordb.core.annotation.date.ExcludeTime;
+import com.jmoordb.core.annotation.date.IncludeTime;
 import com.jmoordb.core.annotation.enumerations.JakartaSource;
+import com.jmoordb.core.annotation.repository.CountBy;
+import com.jmoordb.core.annotation.repository.DeleteBy;
+import com.jmoordb.core.annotation.repository.Find;
+import com.jmoordb.core.annotation.repository.Ping;
+import com.jmoordb.core.annotation.repository.Query;
 import com.jmoordb.core.annotation.repository.Repository;
+import com.jmoordb.core.model.Pagination;
 import com.jmoordb.core.repository.CrudRepository;
 import com.jmoordbcore.capitulo08.model.Animal;
+import com.jmoordbcore.capitulo08.model.Pais;
+import java.time.LocalDateTime;
+import java.util.Date;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Stream;
 
 /**
  *
@@ -15,5 +29,71 @@ import com.jmoordbcore.capitulo08.model.Animal;
  */
 @Repository(entity =Animal.class,jakartaSource = JakartaSource.JAKARTA) 
 public interface AnimalRepository extends CrudRepository<Animal,Long>{
+      @Find
+    public Stream<Animal> findByColorAndApellidoAndEdadLessThanEquals(String color, String apellido, Integer edad);
+    
+    @Query(where = "fechaalimentacion .eq. @fechaalimentacion")
+    public List<Animal> queryByFecha(@IncludeTime Date fechaalimentacion);
+    
+    
+        @Find
+    public List<Animal> findByIEdadGreaterThanPagination(Long edad,Pagination pagination);
+    
+     @Find
+    public List<Pais> findByIdpaisGreaterThanPagination(Long idpais,Pagination pagination);
+    
+
+    @Find
+    public List<Pais> findByFecha(@IncludeTime Date fecha);
+
+    @Find
+    public List<Pais> findByFecha(@ExcludeTime LocalDateTime fecha);
+
+    @Find
+    public List<Pais> findByFechaGreaterThan(Date fecha);
+
+    @Find
+    public List<Pais> findByFechaGreaterThanEquals(@ExcludeTime Date fecha);
+
+    @Find
+    public List<Pais> findByFechaLessThan(@ExcludeTime Date fecha);
+
+    @Find
+    public List<Pais> findByFechaLessThanEquals(@ExcludeTime Date fecha);
+
+    @Find
+    public Optional<Pais> findByFechaAndPais(@ExcludeTime Date fecha, String pais);
+
+    @Find
+    public List<Pais> findByFechaLessThanAndPais(@ExcludeTime Date fecha, String pais);
+
+    @Find
+    public List<Pais> findByPaisAndFechaLessThan(@ExcludeTime Date fecha, String pais);
+
+////    
+//    
+    @Find
+    public List<Pais> findByLocal(@ExcludeTime LocalDateTime local);
+//    
+
+    @CountBy
+    public Long countByLocal(LocalDateTime local);
+
+   
+    @Find
+    public List<Pais> findByFechaGreaterThanEqualsAndFechaLessThanEquals(@IncludeTime Date start, Date end);
+
+
+    @Find
+    public List<Pais> findByFechaGreaterThanEqualAndFechaLessThanEqual(@ExcludeTime Date start, @ExcludeTime Date end);
+
+    @Find
+    public List<Pais> findByFechaGreaterThanEqualAndFechaLessThanEqualAndPais(@ExcludeTime Date start, @ExcludeTime Date end, String pais);
+
+    @DeleteBy
+    public Long deleteByFechaGreaterThan(Date fecha);
+@Ping
+public Boolean ping();
+    
     
 }
