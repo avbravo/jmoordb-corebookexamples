ab55e02ccab5f769f2c669a7b36148032584c8b7
avbravo <avbravo@avbravo-HP-Pavilion-g7-Notebook-PC>
2023-08-16 21:29:42 -0500

    capitulo11

--- capitulo11/run.sh
+++ capitulo11/run.sh
@@ -1,3 +1,19 @@
 mvn clean install payara-micro:bundle
 
 java -jar target/capitulo10-microbundle.jar 
+
+
+ <option>
+                                <key></key>
+                                <value>Djdk.util.zip.disableZip64ExtraFieldValidation=true</value>
+                            </option>
+
+--domainConfig
+
+--
+commandoptions maven agrega el domain.xml
+
+  <option>
+                            <key>--domainconfig</key>
+                            <value>src/main/resources/domain.xml</value>
+                        </option>
