727657113ae8ed73bd69bb1cbf67cb4b073d21a9
avbravo <avbravo@avbravo-HP-Pavilion-g7-Notebook-PC>
2023-08-21 22:17:48 -0500

    CAPItulo14

--- capitulo15/src/test/java/com/capitulo15/test/EstudianteIT.java
+++ capitulo15/src/test/java/com/capitulo15/test/EstudianteIT.java
@@ -9,7 +9,6 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.net.http.HttpResponse.BodyHandlers;
-import java.util.List;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.BeforeEach;
@@ -28,30 +27,14 @@ public class EstudianteIT {
 
     }
 
+    
     @Test
     void findAll() throws Exception {
-  Client client = ClientBuilder.newClient();
-            client.register(authentificationProducer.httpAuthenticationFeature());
-            WebTarget target = client.target(microservicesProducer.microservicesHost() + "/autentificacion/resources/limpieza/findall");
-
-            GenericType<List<Limpieza>> data = new GenericType<List<Limpieza>>() {
-            };
-
-            limpiezaList = target.request(MediaType.APPLICATION_JSON).get(data);
         HttpRequest request = HttpRequest
                 .newBuilder(new URI("http://localhost:8080/api/estudiante"))
                 .build();
         HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
-        assertEquals("[{\"edad\":491,\"idestudiante\":\"1-2-3\",\"nombre\":\"Aristides\"},{\"edad\":25,\"idestudiante\":\"7-8-5\",\"nombre\":\"Maria\"},{\"edad\":25,\"idestudiante\":\"4-3-8\",\"nombre\":\"Luisa\"}]", response.body());
-    }
-    @Test
-    void findAll() throws Exception {
-
-        HttpRequest request = HttpRequest
-                .newBuilder(new URI("http://localhost:8080/api/estudiante"))
-                .build();
-        HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
-        assertEquals("[{\"edad\":491,\"idestudiante\":\"1-2-3\",\"nombre\":\"Aristides\"},{\"edad\":25,\"idestudiante\":\"7-8-5\",\"nombre\":\"Maria\"},{\"edad\":25,\"idestudiante\":\"4-3-8\",\"nombre\":\"Luisa\"}]", response.body());
+        assertEquals("[{\"edad\":49,\"idestudiante\":\"1-2-3\",\"nombre\":\"Aristides\"},{\"edad\":25,\"idestudiante\":\"7-8-5\",\"nombre\":\"Maria\"},{\"edad\":25,\"idestudiante\":\"4-3-8\",\"nombre\":\"Luisa\"}]", response.body());
     }
    
 }
