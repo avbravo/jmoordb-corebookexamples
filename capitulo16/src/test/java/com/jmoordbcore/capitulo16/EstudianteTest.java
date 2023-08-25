e89c74558b76b84de0a5a09245180ccb0c9cc09c
avbravo <avbravo@avbravo-HP-Pavilion-g7-Notebook-PC>
2023-08-24 22:30:36 -0500

    CAPItulo16

---
+++ capitulo16/src/test/java/com/jmoordbcore/capitulo16/EstudianteTest.java
@@ -0,0 +1,35 @@
+package com.jmoordbcore.capitulo16;
+
+import org.eclipse.microprofile.metrics.MetricRegistry;
+import jakarta.inject.Inject;
+import jakarta.ws.rs.client.WebTarget;
+
+import io.helidon.microprofile.tests.junit5.HelidonTest;
+
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+@HelidonTest
+class EstudianteTest {
+
+    @Inject
+    private MetricRegistry registry;
+
+    @Inject
+    private WebTarget target;
+
+    @Test
+    void findByNombre() {
+
+        String result = target
+                .path("estudiante/findbynombre")
+                .queryParam("nombre", "Maria")
+                .request()
+                .get(String.class);
+        assertThat(result, is("[{\"edad\":25,\"idestudiante\":\"7-8-5\",\"nombre\":\"Maria\"}]"));
+
+    }
+
+}
