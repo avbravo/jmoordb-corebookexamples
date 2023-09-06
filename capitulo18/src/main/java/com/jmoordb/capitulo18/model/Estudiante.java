62565ccedb54242c7c62de55cd285369bce5724f
avbravo <avbravo@avbravo-HP-Pavilion-g7-Notebook-PC>
2023-09-05 22:23:43 -0500

    1.0.0.b-2

--- capitulo18/src/main/java/com/jmoordb/capitulo18/model/Estudiante.java
+++ capitulo18/src/main/java/com/jmoordb/capitulo18/model/Estudiante.java
@@ -14,57 +14,7 @@ import jakarta.nosql.Id;
  * @author avbravo
  */
 @Entity(value = "estudiante")
-public class Estudiante {
-
-    @Id("idestudiante")
-    private String idestudiante;
-    @Column
-    private String nombre;
-
-    @Column
-    private Integer edad;
-
-    public Estudiante() {
-    }
-
-    public Estudiante(String idestudiante, String nombre, Integer edad) {
-        this.idestudiante = idestudiante;
-        this.nombre = nombre;
-        this.edad = edad;
-    }
-
-    public String getIdestudiante() {
-        return idestudiante;
-    }
-
-    public void setIdestudiante(String idestudiante) {
-        this.idestudiante = idestudiante;
-    }
-
-    public String getNombre() {
-        return nombre;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public Integer getEdad() {
-        return edad;
-    }
-
-    public void setEdad(Integer edad) {
-        this.edad = edad;
-    }
-
-    @Override
-    public String toString() {
-        return "Estudiante{" + "idestudiante=" + idestudiante + ", nombre=" + nombre + ", edad=" + edad + '}';
-    }
-
-   
-    
-    
+public record Estudiante (@Id("idestudiante")String idestudiante, @Column String nombre, @Column  Integer edad){
     
 
 }
