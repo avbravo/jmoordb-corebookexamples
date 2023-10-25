586b76dfde21754c8943de103b99b3c830c10930
avbravo <avbravo@avbravo-HP-Pavilion-g7-Notebook-PC>
2023-10-25 10:34:19 -0500

    1.0.0.b-6

---
+++ capitulo16/src/test/java/com/jmoordbcore/capitulo16/EstudianteTest.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.jmoordbcore.capitulo16;
+
+import io.helidon.microprofile.testing.junit5.HelidonTest;
+import jakarta.inject.Inject;
+import jakarta.ws.rs.client.WebTarget;
+import org.eclipse.microprofile.metrics.MetricRegistry;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+import org.junit.jupiter.api.Test;
+
+/**
+ *
+ * @author avbravo
+ */
+@HelidonTest
+class EstudianteTest {
+    
+    @Inject
+    private MetricRegistry registry;
+
+    @Inject
+    private WebTarget target;
+
+    @Test
+    void findByNombre() {
+
+
+
+        String result = target
+                .path("estudiante/findbynombre")
+                .queryParam("nombre", "Ana")
+                .request()
+                .get(String.class);
+        assertThat(result, is("[{\"edad\":25,\"idestudiante\":\"7-8-5\",\"nombre\":\"Maria\"}]"));
+
+    }
+}
