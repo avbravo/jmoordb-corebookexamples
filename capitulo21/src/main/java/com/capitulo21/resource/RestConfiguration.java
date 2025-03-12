0a394283e2e852b3e28c8c076a4fc5890d2e497c
avbravo <avbravo@avbravo-IdeaPad-Gaming-3-15ARH7>
2025-03-11 22:48:52 -0500

    javarecord

---
+++ capitulo21/src/main/java/com/capitulo21/resource/RestConfiguration.java
@@ -0,0 +1,12 @@
+package com.capitulo21.resource;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+/**
+ * Configures RESTful Web Services for the application.
+ */
+@ApplicationPath("resources")
+public class RestConfiguration extends Application {
+    
+}
